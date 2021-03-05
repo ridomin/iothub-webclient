e15a0370f6d31594a925a80767064aeaa885ee27
rido-min <rido-min@users.noreply.github.com>
2021-03-05 15:29:48 -0800

    check for components

--- AzIoTHubClient.js
+++ AzIoTHubClient.js
@@ -239,15 +239,14 @@ export /**
  */
 const ackPayload = (propValues, ac, av) => {
   const isObject = o => o === Object(o)
+  const isComponent = o => isObject(o) && (Object.keys(o).filter(k => k === '__t').length === 1)
   const payload = {}
   Object.keys(propValues).filter(k => k !== '$version').forEach(k => {
     const value = propValues[k]
-    if (isObject(value)) {
-      Object.keys(value).filter(k => k !== '__t').forEach(p => {
-        const desiredValue = value[p]
-        propValues[k][p] = { ac, av, value: desiredValue }
-        payload[k] = propValues[k]
-      })
+    const compFound = isComponent(value)
+    if (compFound) {
+      delete value.__t
+      payload[k] = { __t: 'c', ac, av, value }
     } else {
       payload[k] = { ac, av, value }
     }
